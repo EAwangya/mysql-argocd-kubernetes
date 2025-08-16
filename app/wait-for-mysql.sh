7c3f7842f96f93545f1e8f36134b3f693e2df32e
Ernest Awangya <84739332+EAwangya@users.noreply.github.com>
2025-08-16 12:23:29 -0600

    Update wait-for-mysql.sh

--- app/wait-for-mysql.sh
+++ app/wait-for-mysql.sh
@@ -1,8 +1,13 @@
-# node-app/wait-for-mysql.sh
 #!/bin/sh
-echo "⏳ Waiting for MySQL on $DB_HOST:3306 …"
-while ! nc -z "$DB_HOST" 3306; do
+
+DB_HOST=${DB_HOST:-myappdb}
+DB_PORT=${DB_PORT:-3306}
+
+echo "⏳ Waiting for MySQL on $DB_HOST:$DB_PORT …"
+
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
+
 echo "✅  MySQL is up – starting Node app"
 exec "$@"
